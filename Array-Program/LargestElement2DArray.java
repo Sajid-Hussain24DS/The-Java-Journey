16b364a6942582419a464d02e7849ae3fef5d14e
Sajid-Hussain <lasharisajidhussain9@gmail.com>
2024-11-24 13:24:00 +0500

    add

---
+++ Array-Program/LargestElement2DArray.java
@@ -0,0 +1,20 @@
+class LargestElement2DArray{
+    public static void main(String[] args) {
+        int[] a = {90,30,45,83,1000};
+        int max = a[0];
+    for(int i=1; i<a.length;i++){
+        {
+        if(a[i]>max) {
+            {
+                max=a[i];
+                
+        }
+        System.out.println("Maximum Element of Array"+max);
+
+        }
+
+        }
+
+    }
+}
+}
