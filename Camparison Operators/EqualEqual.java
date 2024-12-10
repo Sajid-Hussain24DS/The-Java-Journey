7596af8cc8efac63e628488e17382ed1799b72c7
Sajid Hussain <lasharisajidhussain9@gmail.com>
2024-12-10 18:06:11 +0500

    Add files via upload

---
+++ Camparison Operators/EqualEqual.java
@@ -0,0 +1,21 @@
+class EqualEqual{
+ public static void main(String[] args){
+
+int num1 = 10;
+int num2 = 12;
+int num3 = 33;
+int num4 = 12;
+int num5 = 33;
+int num6 = 32;
+System.out.println(num1==num2);
+System.out.println(num4==num1);
+System.out.println(num4==num2);
+System.out.println(num5==num5);
+System.out.println(num2==num4);
+System.out.println(num3==num2);
+System.out.println(num3==num5);
+System.out.println(num5==num6);
+}
+}
+
+
---
+++ Camparison Operators/greatorEqual.java
@@ -0,0 +1,22 @@
+class greatorEqual{
+ public static void main(String[] args){
+
+int num1 = 5;
+int num2 = 10;
+int num3 = 78;
+int num4 = 67;
+int num5 = 43;
+int num6 = 12;
+System.out.println(num1>=num2);
+System.out.println(num2>=num1);
+System.out.println(num4>=num3);
+System.out.println(num3<=num4);
+System.out.println(num6>=num5);
+System.out.println(num5<=num6);
+System.out.println(num5<=num3);
+System.out.println(num1>=num6);
+}
+
+
+
+}
